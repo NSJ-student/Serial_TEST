6c275c982705c4a58c216299572c11fa010e5be0
NSJ-student <ghdlghdl0518@hanmail.net>
2022-09-15 18:31:56 +0900

    first commit

---
+++ src/GstPlayer.cpp
@@ -0,0 +1,18 @@
+/*
+ * GstPlayer.cpp
+ *
+ *  Created on: 2022. 9. 15.
+ *      Author: Sujin
+ */
+
+#include "GstPlayer.h"
+
+GstPlayer::GstPlayer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GstPlayer::~GstPlayer() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/GstPlayer.h
@@ -0,0 +1,21 @@
+/*
+ * GstPlayer.h
+ *
+ *  Created on: 2022. 9. 15.
+ *      Author: Sujin
+ */
+
+#ifndef SRC_GSTPLAYER_H_
+#define SRC_GSTPLAYER_H_
+
+class GstPlayer {
+public:
+	GstPlayer();
+	virtual ~GstPlayer();
+
+private:
+
+
+};
+
+#endif /* SRC_GSTPLAYER_H_ */
